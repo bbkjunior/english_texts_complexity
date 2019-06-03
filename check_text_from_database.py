84e88303d7c5af88cc134169186021a32e08c0d1
Николай Бабаков <nigula@MacBook-Pro-Nikolaj.local>
2019-06-03 11:24:14 +0300

    adjust calculations

--- check_text_from_database.py
+++ check_text_from_database.py
@@ -2,6 +2,8 @@ from calculate_level_new import get_level_from_raw_text
 import psycopg2
 import json
 from tqdm import tqdm
+import statistics
+
 
 def write_response (json_file, start_index, final_index):
     file_name = './check_results/' + str(start_index) + '-' + str(final_index) +'.json'
@@ -12,8 +14,12 @@ def write_response (json_file, start_index, final_index):
 conn = psycopg2.connect(dbname='pgstage', user='linguist', password='eDQGK0GCStlYlHNV', host='192.168.122.183')
 interval = 100
 #всего страниц в базе 2 262 479
+level2digit = {"Beginner":'0', "Elementary/Pre-Intermediate":'1',"Intermediate":'2',"Upper-Intermediate":'3',"Advanced":'4'}
+digit2level = {'0': "Beginner", '1': "Elementary/Pre-Intermediate",'2':"Intermediate",'3':"Upper-Intermediate",'4':"Advanced"}
+texts_in_one_object = 0
+midle_calc_level = []
 current_text = {"text": '', 'jungle_id':0}
-for offset_ind in tqdm(range (32600,2262500,interval)):
+for offset_ind in tqdm(range (41300,2262500,interval)):
     conn.rollback()
     cursor = conn.cursor()   
     request = "SELECT jdesc ->>'page_text' AS page_text, jungle_id FROM public.content_jungle_pages LIMIT " + str(interval) + " OFFSET " + str(offset_ind)
@@ -22,29 +28,49 @@ for offset_ind in tqdm(range (32600,2262500,interval)):
     level_json = []
     check_index = 0
     #start_index = check_index
-    texts_in_one_object = 0
-    midle_calc_level = []
-    level2digit = {"Beginner":0, "Elementary/Pre-Intermediate":1,"Intermediate":2,"Upper-Intermediate":3,"Advanced":4}
-    cefr2lev = {'0':"Beginner", '1': "Elementary/Pre-Intermediate",'2':"Intermediate",'3':"Upper-Intermediate",'4':"Advanced"}
+    
     for row in cursor:
         if(row[1] != current_text['jungle_id']):
             if len(current_text['text']) > 0:
-                
-                level  = get_level_from_raw_text(current_text['text'])
+                if len(midle_calc_level) > 0:
+                    int_level = int(round(statistics.mean(midle_calc_level),0))
+                    level = digit2level[str(int_level)]
+                    midle_calc_level = []
+                elif len(midle_calc_level) == 0:
+                    level  = get_level_from_raw_text(current_text['text'])
                 level_json.append({"jungle_id": current_text['jungle_id'], "level":level})
                 print(current_text['text'], current_text['jungle_id'], "LEVEL CALCULATED AS",level, "\n")
                 print("====NEW TEXT (previous texts calculations are above)====\n")
                 current_text = {"text": row[0], 'jungle_id':row[1]}
+                texts_in_one_object = 1
             else:
-                print("====FIRST ENTRY====\n")#в середине текста может возникнуть если предыдущий текст был пустым
-                current_text['jungle_id'] = row[1]
-                current_text['text'] += ' ' + row[0]
+                if len(midle_calc_level) > 0:
+                    int_level = int(round(statistics.mean(midle_calc_level),0))
+                    level = digit2level[str(int_level)]
+                    level_json.append({"jungle_id": current_text['jungle_id'], "level":level})
+                    print(current_text['text'], current_text['jungle_id'], "LEVEL CALCULATED AS",level, "\n")
+                    print("====NEW TEXT (previous texts calculations are above FROM LIST OF COMPLEXITIES)====\n")
+                    current_text = {"text": row[0], 'jungle_id':row[1]}
+                    texts_in_one_object = 1
+                    midle_calc_level = []
+                else:
+                    print("====FIRST ENTRY====\n")#в середине текста может возникнуть если предыдущий текст был пустым
+                    texts_in_one_object += 1
+                    current_text['jungle_id'] = row[1]
+                    current_text['text'] += ' ' + row[0]
         else:
-	   if texts_in_one_object >5:
-            print("====ADD TEXT TO EXISTING====\n")
-            current_text['text'] += ' ' + row[0]
-        #print(current_text)     
-        
+            if texts_in_one_object <5:
+                print("====ADD TEXT TO EXISTING====\n")
+                current_text['text'] += ' ' + row[0]
+                texts_in_one_object += 1
+            else:
+                print("====TOO MUCH TEXT INFO INSIDE ONE OBJECT CALCULATE AVERAGE COMPLEXITY====\n")
+                level  = get_level_from_raw_text(current_text['text'])
+                current_text['text'] = ''
+                int_level = int(level2digit[level])
+                midle_calc_level.append(int_level)
+                print("CURRENT COMPLEXITY LIST IS",midle_calc_level)
+                texts_in_one_object = 0
         check_index += 1
         
     #if check_index % 250 == 0 and check_index != 0:
